c13314411db07219babf4540ec10728376d06705
Prudhvi <gadiraju.prudhvi@gmail.com>
2020-07-28 16:07:59 -0700

    Initial Commit

---
+++ Shared/CoreDataPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataPracticeApp.swift
+//  Shared
+//
+//  Created by Prudhvi Gadiraju on 7/28/20.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
